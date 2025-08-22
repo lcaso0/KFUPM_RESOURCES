b60292c322882c3c6a27cdfa68a783136d06295c
lcaso0 <jawad118877@gmail.com>
2025-08-22 22:08:18 +0300

    Change file structure

--- app/resources/[communityId]/page.tsx
+++ app/resources/[communityId]/page.tsx
@@ -1,5 +1,31 @@
+import db from "@/db";
+import { communities } from "@/db/schema";
+import { eq } from "drizzle-orm";
 import ResourcesList from "./components/ResourcesList";
 
-export default async function CommunityPage() {
+interface Params {
+  children: React.ReactNode;
+  params: Promise<{ communityId: string }>;
+}
+
+export default async function CommunityPage({ params }: Params) {
+  const communityId = await params.then(
+    (p: { communityId: string }) => p.communityId,
+  );
+  console.log("Community Layout, communityId:", communityId);
+
+  const community = await db.query.communities.findFirst({
+    where: eq(communities.id, communityId),
+    with: {
+      folders: true
+    }
+  });
+
+  if (!community) {
+    return <div>Community not found</div>;
+  }
+
+  console.log("Community:", community);
+
   return <ResourcesList />;
 }
